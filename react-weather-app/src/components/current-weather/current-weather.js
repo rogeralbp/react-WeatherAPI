07edc3e48e5bac9d95f645a6bc60d2ec7b312839
Roger Oporta <roportaperez@gmail.com>
2022-07-18 11:04:51 -0600

    Real Time current forecast completed

--- react-weather-app/src/components/current-weather/current-weather.js
+++ react-weather-app/src/components/current-weather/current-weather.js
@@ -8,16 +8,45 @@ const CurrentWeather = () => {
         <div className="weather">
 
             <div className="top">
-
                 <div>
                     <p className="city">Belgrade</p>
                     <p className="weatherDescription">Sunny</p>
                 </div>
-
                 <img alt="weather" className="weather-icon" src="icons/01d.png" />
-
             </div>
 
+            <div className="bottom">
+
+                <p className="temperature">30°C</p>
+
+                <div className="details">
+
+                    <div className="parameter-row">
+                        <span className="parameter-label"> Details </span>
+                    </div>
+
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels Like </span>
+                        <span className="parameter-value">36°C</span>
+                    </div>
+
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind </span>
+                        <span className="parameter-value">4 m/s</span>
+                    </div>
+
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity </span>
+                        <span className="parameter-value">15%</span>
+                    </div>
+
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure </span>
+                        <span className="parameter-value">15 hPa</span>
+                    </div>
+
+                </div>
+            </div>
 
 
         </div>
